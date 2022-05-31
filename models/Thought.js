ad077a549667048dea8de98ce03f7104f6271903
Legolas <geran.vanatta@gmail.com>
2022-05-31 14:10:53 -0500

    thought model done

--- models/Thought.js
+++ models/Thought.js
@@ -1,38 +1,68 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+          },
+          username: {
+            type: String,
+            required: true,
+          },
+          createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+          },
+        },
+        {
+          toJSON: {
+            getters: true,
+        },
+        _id: false,
+    }
+);
+
 const ThoughtSchema = new Schema({
-    thoughtname: {
+    thoughtText: {
         type: String,
         required: true,
-        trim: true
-    },
-    createdBy: {
-        type: String,
-        required: true,
-        trim: true
+        minlength: 1,
+        maxlength: 280,
     },
     createdAt: {
         type: Date,
         default: Date.now,
-        get: (createdAtVal) => dateFormat(createdAtVal)
-    }
-},
+        get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [ReactionSchema],
+    },
     {
       toJSON: {
         virtuals: true,
+        getters: true,
       },
-      id: false
+      id: false,
     }
 );
 
 // get total count of comments and replies on retrieval
 ThoughtSchema.virtual('reactionCount').get(function() {
-    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+    return this.reactions.length;
 });
 
-// create the pizza model using PizzaSchema
 const Thought = model('Thought', ThoughtSchema);
 
-// export the pizza model
 module.exports = Thought;
